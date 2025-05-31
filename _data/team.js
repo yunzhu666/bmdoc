9f65fd5e0a93271b20f7a1dd2c1363fa1fbf5443
云竹 <103818648+yunzhu666@users.noreply.github.com>
2025-05-31 13:31:12 +0800

    Update team.js
    修改内容

--- _data/team.js
+++ _data/team.js
@@ -1,31 +1,17 @@
 export const core = [
   {
+    avatar: 'https://i1.hdslb.com/bfs/face/8d3114d750c12d52ac9c3071c1f65a51a27a4e70.jpg@60w_60h.webp',
+    name: '笔墨星河AI写作',
+    title: '创始人',
+    org: '笔墨星河',
+    desc: '笔墨绘星河，书写属于自己的故事！',
+  },
+  {
     avatar: 'http://q.qlogo.cn/headimg_dl?dst_uin=2457483710&spec=640&img_type=jpg',
     name: '云竹',
-    title: '创始人',
-    org: 'Cloudery',
-    desc: '你好，这里是 YZ ，24岁，是学生，对有趣的世界和可能有趣的你感到好奇，热爱Tech和ACG文化，正在尝试从事互联网产品/Web开发相关工作。',
-  },
-  {
-    avatar: 'http://q.qlogo.cn/headimg_dl?dst_uin=2675256772&spec=640&img_type=jpg',
-    name: '柒屹',
     title: '成员',
     org: 'Cloudery',
-    desc: '我是柒屹，同时也是云术工作室重要成员之一，屹立不倒。从事互联网事业及Web开发工作，也在搞关于Java的一些东西',
-  },
-  {
-    avatar: 'http://q.qlogo.cn/headimg_dl?dst_uin=3014786442&spec=640&img_type=jpg',
-    name: 'ZERO(0)',
-    title: '成员',
-    org: 'Cloudery',
-    desc: '管理云术生存战争服务器，同时也是云术工作室重要成员之一。',
-  },
-  {
-    avatar: 'http://q.qlogo.cn/headimg_dl?dst_uin=953595252&spec=640&img_type=jpg',
-    name: '信念',
-    title: '成员',
-    org: 'Cloudery',
-    desc: '管理云术生存战争服务器，同时也是云术工作室重要成员之一。',
+    desc: '我是YZ',
   },
 ]
 
