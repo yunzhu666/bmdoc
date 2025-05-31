7b991cc8d7519c5caacc814e201688e0e3a0b1b5
云竹 <103818648+yunzhu666@users.noreply.github.com>
2025-05-31 13:39:48 +0800

    Update team.js
    改头像

--- _data/team.js
+++ _data/team.js
@@ -1,6 +1,6 @@
 export const core = [
   {
-    avatar: 'https://i1.hdslb.com/bfs/face/8d3114d750c12d52ac9c3071c1f65a51a27a4e70.jpg@60w_60h.webp',
+    avatar: './bm.svg',
     name: '笔墨星河AI写作',
     title: '创始人',
     org: '笔墨星河',
